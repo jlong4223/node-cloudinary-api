494c5d744e88fd75423f5e7457be13c183e76565
Jared Long <jlong4223@gmail.com>
2021-06-11 13:43:06 -0500

    chore: creates function to connect to cloudinary

---
+++ config/cloudinaryConfig.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
+const Console = require("Console");
+
+(async () => {
+  try {
+    await cloudinary.config({
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.API_KEY,
+      api_secret: process.env.API_SECRET,
+    });
+
+    (() => {
+      switch (true) {
+        case cloudinary.config().api_key !== null &&
+          cloudinary.config().cloud_name !== null &&
+          cloudinary.config().api_secret !== null:
+          return Console.success(
+            `⚡️ ⚡️ ⚡️ CloudinaryConnectionStatus: 200, connected to: ${
+              cloudinary.config().cloud_name
+            } ⚡️ ⚡️ ⚡️`
+          );
+        default:
+          return Console.warning("Something is wrong with cloudinary configs");
+      }
+    })();
+  } catch (err) {
+    Console.error(`🚧 == Cloudinary config error: ${err} == 🚧 `);
+  }
+})();
