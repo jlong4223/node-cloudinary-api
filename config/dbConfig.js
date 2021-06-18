17cc9cd20a7dee2840fd778e64d60b56ba1c8257
Jared Long <jlong4223@gmail.com>
2021-06-18 15:26:01 -0500

    chore: connects to mongodb cloud

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,1 +1,16 @@
-// TODO connect to mongoDB atlas
+const mongoose = require("mongoose");
+const Console = require("Console");
+
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+console.log(db);
+
+db.once("connected", () => {
+  Console.success(`🐸 -> Connected to MongoDB on ${db.host}: ${db.port} <- 🐸`);
+});
