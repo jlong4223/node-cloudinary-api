d9fb1e41f06f4309d89e6d4236064cc940f542a2
Jared Long <jlong4223@gmail.com>
2021-06-24 09:51:55 -0500

    note: adds todo note for finishing crud

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const userCtrl = require("../controllers/user");
 const multerImgCtrl = require("../controllers/multer");
 
 router.get("/users", userCtrl.getUsers);
-
+// TODO have a delete and put route with cloudinary
 router.post(
   "/signup",
   multerImgCtrl.uploadImage.single("image"),
