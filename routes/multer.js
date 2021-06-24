ae31245d405364b7946b5653ade0dc04cefa0942
Jared Long <jlong4223@gmail.com>
2021-06-24 09:51:19 -0500

    adds helpful note for naming reason

--- routes/multer.js
+++ routes/multer.js
@@ -6,6 +6,7 @@ const imgCtrl = require("../controllers/multer");
 // route saves the images directly to this servers image file
 router.post(
   "/upload",
+  // image in quotes has to match the key name that is sending the file
   imgCtrl.uploadImage.single("image"),
   (req, res) => {
     res.send(req.file);
