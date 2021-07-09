8495dc0fedc47ae506fa6eeddd58758251b542ad
Jared Long <jlong4223@gmail.com>
2021-07-09 12:21:03 -0400

    refactor: shortens all route urls and removes single file route

--- routes/multiApp.js
+++ routes/multiApp.js
@@ -5,9 +5,9 @@ const router = express.Router();
 const appCtrl = require("../controllers/multiApp/multiApp");
 const multerImgCtrl = require("../controllers/multer");
 
-router.get("/allapps/data", appCtrl.showAllMultiAppData);
-router.get("/allapps/data/:app", appCtrl.getSpecificAppData);
-router.get("/allapps/data/:app/:userId", appCtrl.getSpecificAppUserData);
+router.get("/allapps", appCtrl.showAllMultiAppData);
+router.get("/allapps/:app", appCtrl.getSpecificAppData);
+router.get("/allapps/:app/:userId", appCtrl.getSpecificAppUserData);
 
 // This route will create user data and their first image but if a user already exists and tries to hit this route, this route will essentially behave like the below post route and just add the picture to the users existing data
 router.post(
@@ -18,23 +18,23 @@ router.post(
 
 // TODO remove this appCtrl function and replace it with addMultiplePicsToUser()
 // this route allows for a user to add additional pictures to their data
-router.post(
-  "/allapps/data/:app/:userId",
-  multerImgCtrl.uploadImage.single("image"),
-  appCtrl.addToUsersPicData
-);
+// router.post(
+//   "/allapps/:app/:userId",
+//   multerImgCtrl.uploadImage.single("image"),
+//   appCtrl.addToUsersPicData
+// );
 
 // this route allows a user to add multiple pictures to their data
 router.post(
-  "/allapps/data/:app/:userId/multi",
+  "/allapps/:app/:userId",
   multerImgCtrl.uploadImage.array("image", 5),
   appCtrl.addMultiplePicsToUser
 );
 
 // this route deletes a user's data and all of their images
-router.delete("/allapps/data/:app/:userId", appCtrl.deleteUserAndPicData);
+router.delete("/allapps/:app/:userId", appCtrl.deleteUserAndPicData);
 
 // this route allows for a user to delete their own pic data
-router.delete("/allapps/data/:app/:userId/:picId", appCtrl.deleteUserPicData);
+router.delete("/allapps/:app/:userId/:picId", appCtrl.deleteUserPicData);
 
 module.exports = router;
