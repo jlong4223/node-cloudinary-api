5432e1bc6ef13a36f69eea2713718c3f349f5b69
Jared Long <jlong4223@gmail.com>
2021-07-13 16:32:04 -0400

    removed duplicate schemas and imports instead

--- models/multiFrontend.js
+++ models/multiFrontend.js
@@ -1,21 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-// const picSchema = require("./picModel");
-
-const picSchema = new Schema(
-  {
-    image: String,
-    cloudinaryID: String,
-  },
-  {
-    timestamps: true,
-  }
-);
+const picModelSchema = require("./picModel").schema;
 
 const appSchema = new Schema({
   userID: String,
   application: String,
-  picture: [picSchema],
+  picture: [picModelSchema],
 });
 
 module.exports = mongoose.model("MultiFrontend", appSchema);
